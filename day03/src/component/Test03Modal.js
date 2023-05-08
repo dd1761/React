6ef45c99683783e9120df21f4a458df01cb8f5e8
KIMDONGBEEN <dd1761@naver.com>
2023-05-08 17:44:31 +0900

    23-05-08

---
+++ day03/src/component/Test03Modal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Test03Modal = () => {
+    return (
+        <>
+            <div className='bg'></div>   
+            <div className='popup'>
+                <p className='closex' style={{cursor: 'pointer'}}>X</p>
+                <h2>Have a nice day!!</h2>
+            </div>
+        </>
+    );
+};
+
+export default Test03Modal;
