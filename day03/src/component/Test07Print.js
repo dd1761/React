9f0c9cdd1798fe2cbe03e77abec6b5889aea6224
KIMDONGBEEN <dd1761@naver.com>
2023-05-09 17:14:18 +0900

    23-05-09
    Test07 완성
    main화면에서 input print output까지 연결하며 화면 이동

--- day03/src/component/Test07Print.js
+++ day03/src/component/Test07Print.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const Test07Print = () => {
+const Test07Print = ({name, age, addr, phone, onPrev, onNext}) => {
+    
     return (
-        <div>
+        <>
             
-        </div>
+            <ul>
+                <li>이름 : <em>{name}</em></li>
+                <li>나이 : <em>{age}</em></li>
+                <li>주소 : <em>{addr}</em></li>
+                <li>핸드폰 : <em>{phone}</em></li>
+            </ul>
+            <p>
+                <button onClick={onPrev}>이전</button> &nbsp;
+                <button onClick={onNext}>다음</button>
+            </p>
+            
+        </>
     );
 };
 
