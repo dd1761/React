085fac663d52e319c626f7ae032f051f8d28ffc6
KIMDONGBEEN <dd1761@naver.com>
2023-05-10 15:15:30 +0900

    23-05-10
    Test09에 고양이 사진들과 import추가

--- day03/src/component/Test09.js
+++ day03/src/component/Test09.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import img01 from '../img/cat01.jpg';
+
 const Test09 = () => {
     return (
         <div>
-            
+            <img src='./'/>
         </div>
     );
 };
 
 export default Test09;
+
+/*
+src 안에 있는 이미지 파일 처리는 참조변수를 사용한다.
+  import 참조변수 from '이미지경로';
+*/
