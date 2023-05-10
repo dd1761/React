1cce210c54f55bb583949d00620b5e93e0456fe9
KIMDONGBEEN <dd1761@naver.com>
2023-05-10 16:41:04 +0900

    23-05-10

--- day03/src/App.js
+++ day03/src/App.js
@@ -8,6 +8,8 @@ import Test06 from './component/Test06';
 import Test07Main from './component/Test07Main';
 import Test08Gallery from './component/Test08Gallery';
 import Test09 from './component/Test09';
+import Test10 from './component/Test10';
+import Test11 from './component/Test11';
 
 function App() {
   return (
@@ -20,7 +22,9 @@ function App() {
       {/* <Test06 /> */}
       {/* <Test07Main /> */}
       {/* <Test08Gallery /> */}
-      <Test09 />
+      {/* <Test09 /> */}
+      {/* <Test10 /> */}
+      <Test11 />
     </div>
   );
 }
---
+++ day04/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Test01 from './component/Test01';
+
+const App = () => {
+  return (
+    <div>
+      <Test01 />
+    </div>
+  );
+};
+
+export default App;
+
