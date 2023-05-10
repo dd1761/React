5bdb5ad0fbb575e58b69d51dc77b3ead5d35f570
KIMDONGBEEN <dd1761@naver.com>
2023-05-10 17:55:26 +0900

    23-05-10

--- day04/src/component/Test02Sub.js
+++ day04/src/component/Test02Sub.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Test02Sub = () => {
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
+
+    const onMove = (e) => {
+        //브라우저에서 사용자가 웹페이지가 보여지는 영역을 기준으로 좌표를 표시
+        setX(e.clientX);
+        setY(e.clientY);
+    }
+
+    useEffect(() => {   //이 컴포넌트가 처음 나타날 때만 한번 실행
+        console.log('useEffect');
+        window.addEventListener('mousemove', onMove);
+
+        return () => {
+            console.log('clean up');
+            window.removeEventListener('mousemove', onMove);
+        }
+    }, []) 
+
     return (
         <div>
             <h2>마우스 좌표</h2>
