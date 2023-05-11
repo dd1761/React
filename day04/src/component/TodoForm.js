24ee0cbc57549524bc62d08203446b5a4905f19b
KIMDONGBEEN <dd1761@naver.com>
2023-05-11 12:30:22 +0900

    23-05-11
    데이터 값 저장과 포커스 맞추기

--- day04/src/component/TodoForm.js
+++ day04/src/component/TodoForm.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import styles from '../css/TodoForm.module.css';
 
-const TodoForm = () => {
+const TodoForm = ({ onAdd }) => {
+
+    const textRef = useRef()
+
+    const [text, setText] = useState('');
+
+    const onInput = (e) => {
+        setText(e.target.value);
+        // const onInput = (e) => {
+        //     const { value } = e.target
+        //     setText(value)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if(!text) return
+        
+        onAdd(text);
+
+        setText('');
+
+        textRef.current.focus()
+    }
+
+    
+
     return (
         <div>
-            
+            <form className={ styles.TodoForm } onSubmit={ onSubmit } >
+                <input type='text' value={ text } placeholder='해야 할 일을 입력하시오' onInput={ onInput } ref={textRef}/>
+                <button type='submit' onSubmit={ onSubmit } >추가</button>
+            </form>
         </div>
     );
 };
--- day04/src/component/Todos.js
+++ day04/src/component/Todos.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import '../css/Todos.module.css';
+import React, { useState } from 'react';
+import styles from '../css/Todos.module.css';
+import TodoForm from './TodoForm';
 
 const Todos = () => {
+    const [data, setData] = useState([]);
+
+    const onAdd = (text) => {
+        setData([
+            ...data,
+            {
+                text: text
+            }
+        ]);
+    }
+
     return (
-        <div>
-            
+        <div className={ styles.Todos }>
+            <h1>일정관리</h1>
+            <TodoForm onAdd={onAdd}/>
         </div>
     );
 };
