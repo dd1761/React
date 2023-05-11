7d82c835004bf04091fffadb02aacca611439038
KIMDONGBEEN <dd1761@naver.com>
2023-05-11 16:53:52 +0900

    23-05-11

--- day05/src/App.js
+++ day05/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Test01 from './component/Test01';
 import Test02 from './component/Test02';
 import Test03 from './component/Test03';
+import Test04 from './component/Test04';
 
 const App = () => {
   return (
     <div>
       {/* <Test01 /> */}
       {/* <Test02 /> */}
-      <Test03 />
+      {/* <Test03 /> */}
+      <Test04 />
     </div>
   );
 };
