3dc6022649b4ea2e6fc70e0a2cd5167ed1001b7e
KIMDONGBEEN <dd1761@naver.com>
2023-05-15 12:42:25 +0900

    23-05-15
    day07 page04

--- day07/src/App04.js
+++ day07/src/App04.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Main from './page04/Main';
+import MemberDetail from './page04/MemberDetail';
 
 const App04 = () => {
+    
     return (
-        <div>
-            
-        </div>
+        <BrowserRouter>
+            <>
+                {/* 화면에 보이는 영역입니다. */}
+                <Routes>
+                    <Route path='/' element={ <Main/> }/>
+                    <Route path='/member'>
+                        <Route path=':memberId' element={ <MemberDetail /> } />
+                    </Route>
+                </Routes>
+            </>
+        </BrowserRouter>
     );
 };
 
--- day07/src/page04/MemberDetail.js
+++ day07/src/page04/MemberDetail.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const MemberDetail = () => {
+    const { memberId } = useParams()
+
+    const css = {
+        border: '1px solid cyan', margin: 20, padding: 20
+    }
     return (
         <div>
             
