d892275e3bf6dd4babcd610eb786aa945f96fb75
KIMDONGBEEN <dd1761@naver.com>
2023-05-15 14:14:36 +0900

    23-05-15
    day07 MemberDetail에서
    1인분의 데이터를 받아와 화면에 출력

--- day07/src/page04/MemberDetail.js
+++ day07/src/page04/MemberDetail.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const MemberDetail = () => {
     const { memberId } = useParams()
+    const [member, setMember] = useState({});
+    const { name, email, phone, website } = member;
+
+    useEffect(() => {
+        axios
+            .get(`https://jsonplaceholder.typicode.com/users/${memberId}`)
+            .then(res => setMember(res.data));
+    }, []);
 
     const css = {
         border: '1px solid cyan', margin: 20, padding: 20
     }
     return (
-        <div>
-            
+        <div style={css}>
+            <h2>MemberDetail Page : {memberId}</h2>
+            <h4>이름 :  {name}</h4>
+            <ul>
+                <li>이메일 : {email}</li>
+                <li>전화번호 : {phone}</li>
+                <li>웹사이트 : {website}</li>
+            </ul>
         </div>
     );
 };
