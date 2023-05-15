08218b32a4ba98996fdc7067b08ef4a847587c22
KIMDONGBEEN <dd1761@naver.com>
2023-05-15 14:25:37 +0900

    23-05-15
    navigator사용

--- day07/src/page04/Member.js
+++ day07/src/page04/Member.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import MemberDetail from './MemberDetail';
 
 const Member = ({ item }) => {
     const { id, name, email } = item
 
+    const navigate = useNavigate()
+
+
     const css = {
         border: '1px solid cyan', margin: '20px', padding: 20
+    }
+
+    const onGO = () => {
+        navigate(`/member/${id}`)
     }
 
     return (
@@ -15,6 +23,7 @@ const Member = ({ item }) => {
             <h5>이메일 : {email}</h5>
 
             <p><Link to={`/member/${id}`}>자세히 보기</Link></p>
+            <button onClick={ onGO }>상세보기</button>
         </div>
     );
 };
--- day07/src/page04/MemberDetail.js
+++ day07/src/page04/MemberDetail.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const MemberDetail = () => {
     const { memberId } = useParams()
     const [member, setMember] = useState({});
     const { name, email, phone, website } = member;
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         axios
@@ -16,6 +18,12 @@ const MemberDetail = () => {
     const css = {
         border: '1px solid cyan', margin: 20, padding: 20
     }
+
+    const onBack = () => {
+        // navigate('/')
+        navigate(-1)
+    }
+
     return (
         <div style={css}>
             <h2>MemberDetail Page : {memberId}</h2>
@@ -25,6 +33,7 @@ const MemberDetail = () => {
                 <li>전화번호 : {phone}</li>
                 <li>웹사이트 : {website}</li>
             </ul>
+            <button onClick={ onBack }>뒤로</button>
         </div>
     );
 };
